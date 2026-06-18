836d70488afb06bdbd988bc30b0336030b5e8d8a
luxudux <luxudux@gmail.com>
2026-06-18 16:22:41 -0600

    bump: actualizar a la version 1.0.0

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const version = 2.0;
+const { version } = require('./package.json');
 
 app.get('/', (req, res) => {
   //res.send(`<h1>🚀 Versión ${version} de la aplicación - Estable y Funcionando</h1>`);
