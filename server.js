efd70943f1bf962077449f59d8e4de9b2afa1397
luxudux <luxudux@gmail.com>
2026-06-19 15:03:55 -0600

    fix: modificando leyenda de nuevo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 const { version } = require('./package.json');
 
 app.get('/', (req, res) => {
-  res.send(`<h1>🚀 Versión ${version} de la aplicación - Estable y Funcionando desde DOCKER HUB automatico</h1>`);
+  res.send(`<h1>🚀 Versión ${version} de la aplicación - Estable y Funcionando desde DOCKER HUB automatico con github actions</h1>`);
   //res.send(`<h1>🚨 Versión ${version} - TODO ESTÁ ROTO EN PRODUCCIÓN desde DOCKER HUB</h1>`);
 });
 
